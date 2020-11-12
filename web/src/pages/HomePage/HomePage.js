a45f93692b6b03efca0e0e136fbfc0c6a8d548ee
Dusty McCord <Dustymccord@gmail.com>
2020-11-12 09:26:10 -0800

    add home page, about page, and layout.

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const HomePage = () => {
+  return (
+    <BlogLayout>
+      <h1>Home</h1>
+      <p>
+        This site was created to demonstrate my mastery of Redwood: Look on my
+        works, ye mighty, and despair!
+      </p>
+    </BlogLayout>
+  )
+}
+
+export default HomePage
